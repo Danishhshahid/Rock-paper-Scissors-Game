71aa34b9d825556e6be6236edbbb39cadece4fec
Danish Shahid <Danishhshahid@gmail.com>
2024-04-29 16:00:59 +0500

    Game

---
+++ Rock paper scisssor/main.ts
@@ -0,0 +1,30 @@
+// Rock Paper Scissor Assignment from Miss Hina
+//Date 29- Apr-2024 //Sunday
+
+function choices(choice1: string, choice2: string) {
+  if (choice1 === choice2) {
+    return "It's a tie!";
+  } else if (choice1 == "rock") {
+    if (choice2 == "scissors") {
+      return `Rock Wins`;
+    } else {
+      return `Paper Wins`;
+    }
+  } else if (choice1 === "paper") {
+    if (choice2 === "rock") {
+      return `Paper Wins`;
+    } else {
+      return `Scissors wins`;
+    }
+  } else if (choice1 == "scissors") {
+    if (choice2 == "paper") {
+      return `Scissors Wins`;
+    } else {
+      return `Rock Wins`;
+    }
+  }
+}
+
+console.log(choices("rock", "scissors"));// rocks wins
+console.log(choices("paper", "scissors"));// scissors wins
+console.log(choices("rock", "paper"));// paper wins
